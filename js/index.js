4b79a77bd4371315b898240343bce7ade88273d6
Rodrigo Cabrera <rodrigo.cabrera@innew.la>
2022-05-27 20:58:12 -0300

    finished de modal success e inicio de modal error

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
 window.onload = function () {
-  const openButton = document.querySelector(".open-button");
+  const openButton = document.querySelectorAll(".open-button");
   const closeButton = document.querySelectorAll(".close-button");
   const modalWarning = document.getElementById("warning-container");
+  const modalSuccess = document.getElementById("success-container");
 
-  openButton.addEventListener("click", handleModals);
+  openButton.forEach(function (e) {
+    e.addEventListener("click", () => handleModals(e));
+  });
   closeButton.forEach(function (e) {
-    e.addEventListener("click", handleModals);
+    e.addEventListener("click", () => handleModals(e));
   });
 
   function handleModals(e) {
     if (e.classList.contains("open-warning")) {
       modalWarning.classList.toggle("modal-active");
+      modalSuccess.classList.remove("modal-active");
+    }
+    else if (e.classList.contains("open-success")) {
+      modalSuccess.classList.toggle("modal-active");
+      modalWarning.classList.remove("modal-active");
+    } else {
+      modalWarning.classList.remove("modal-active");
+      modalSuccess.classList.remove("modal-active");
     }
   }
 };
